7e47b740a2bb74b0e35adb91b74a62464538cb9f
Husain-Jinia <75819874+Husain-Jinia@users.noreply.github.com>
2021-04-04 11:52:54 +0530

    Initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">About Us</a>
+                <a href="#">Contact Us</a>
+            </nav>
+        </header>
+    )
+}
