4c4bd19035bed1908547feb39d2d5de348f70687
Husain-Jinia <75819874+Husain-Jinia@users.noreply.github.com>
2021-04-05 17:04:04 +0530

    Setting up the pages (commit 3 )

---
+++ src/components/BlogPost/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Card from '../UI/Card'
+import './style.css'
+
+function BlogPost(props) {
+    return (
+        <div>
+            <Card>My first blog Post</Card>
+        </div>
+    )
+}
+
+export default BlogPost
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
+import {NavLink} from 'react-router-dom';
 import './style.css'
 
 function Navbar() {
+
+    const[search,setSearch] = useState(false);
+
+    function submitSearch(e) {
+        e.preventDefault();
+        alert('Searched')
+    }
+
+    const openSearch = () =>{
+        setSearch(true);
+    }
+
+    const searchClass = search?"searchinput active":"searchinput";
+
     return (
         <div className="navbar">
             <ul className="navbarMenu">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Posts</a></li>
-                <li><a href="#">Contact Us</a></li>
+                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink to="/about-us">About Us</NavLink></li>
+                <li><NavLink to="/post">Posts</NavLink></li>
+                <li><NavLink to="/contact-us">Contact Us</NavLink></li>
             </ul>
             <div className="search">
-                <input type="text" placeholder="Search" />
-                <img src={require('../../assets/icons/search.png')} alt="Search" />
+                <form onSubmit={submitSearch}>
+                <input type="text" className={searchClass} placeholder="Search" />
+                <img onclick={openSearch} className="searchicon" src={require('../../assets/icons/search.png')} alt ="Search"/>
+                </form>
+                
             </div>
         </div>
 
